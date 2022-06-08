f742cad64ff69e03f608b358748d68cc0283155c
Ewan Morrison <ewanmorrrison@gmail.com>
2022-06-08 17:40:59 +0100

    feat: add caching to castle instantiation

--- src/main/kafka/getCastle.ts
+++ src/main/kafka/getCastle.ts
@@ -1,35 +1,44 @@
 import { Castle, createCastle } from "@ovotech/castle";
 import { ConnectionConfig } from "renderer/pages/connections/Connections";
 
+const castleClientCache: {
+  [connectionName: string]: Castle;
+} = {};
+
 export const getCastle = async (config: ConnectionConfig): Promise<Castle> => {
-  const castle = createCastle({
-    schemaRegistry: {
-      uri: config.schemaUri.replace(
-        "https://",
-        `https://${config.schemaUsername}:${config.schemaPassword}@`
-      ),
-    },
-    kafka: {
-      brokers: [config.clusterUri],
-      connectionTimeout: 10000,
-      ...(config.type === "ssl"
-        ? {
-            ssl: {
-              rejectUnauthorized: false,
-              ca: [config.ca],
-              key: config.key,
-              cert: config.cert,
-            },
-          }
-        : {
-            sasl: {
-              username: config.username,
-              mechanism: config.mechanism,
-              password: config.password,
-            },
-          }),
-    },
-  });
-  await castle.start();
-  return castle;
+  if (castleClientCache[config.connectionName]) {
+    return castleClientCache[config.connectionName];
+  } else {
+    const castle = createCastle({
+      schemaRegistry: {
+        uri: config.schemaUri.replace(
+          "https://",
+          `https://${config.schemaUsername}:${config.schemaPassword}@`
+        ),
+      },
+      kafka: {
+        brokers: [config.clusterUri],
+        connectionTimeout: 5000,
+        ...(config.type === "ssl"
+          ? {
+              ssl: {
+                rejectUnauthorized: false,
+                ca: [config.ca],
+                key: config.key,
+                cert: config.cert,
+              },
+            }
+          : {
+              sasl: {
+                username: config.username,
+                mechanism: config.mechanism,
+                password: config.password,
+              },
+            }),
+      },
+    });
+    await castle.start();
+    castleClientCache[config.connectionName] = castle;
+    return castle;
+  }
 };

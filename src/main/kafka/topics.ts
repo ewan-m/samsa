8bac33d690877e35533d79fb419a677db35eb09f
Ewan Morrison <ewanmorrrison@gmail.com>
2022-06-08 17:48:36 +0100

    feat: add topic info fetching abilities

--- src/main/kafka/topics.ts
+++ src/main/kafka/topics.ts
@@ -8,3 +8,34 @@ export const getTopics = async (
 
   return await castle.kafka.admin().listTopics();
 };
+
+export const getTopicConfig = async (
+  config: ConnectionConfig,
+  topicName: string
+): Promise<any> => {
+  const castle = await getCastle(config);
+
+  return (
+    await castle.kafka.admin().describeConfigs({
+      resources: [{ type: 2, name: topicName }],
+      includeSynonyms: true,
+    })
+  ).resources[0].configEntries;
+};
+
+export type TopicOffset = {
+  partition: number;
+  offset: string;
+};
+
+export const getTopicOffsets = async (
+  config: ConnectionConfig,
+  topicName: string,
+  timestamp: number = new Date().getTime()
+): Promise<TopicOffset[]> => {
+  const castle = await getCastle(config);
+
+  return await castle.kafka
+    .admin()
+    .fetchTopicOffsetsByTimestamp(topicName, timestamp);
+};
--- src/main/preload.ts
+++ src/main/preload.ts
@@ -3,7 +3,7 @@ import { contextBridge } from "electron";
 import { getAwsSecrets } from "./aws/getAwsSecrets";
 import { getAwsSsmParams } from "./aws/getAwsSsmParams";
 import { getSubjects, getSubjectValue } from "./kafka/subjects";
-import { getTopics } from "./kafka/topics";
+import { getTopicConfig, getTopicOffsets, getTopics } from "./kafka/topics";
 import { prettify } from "./prettify/prettify";
 
 const exposedApi = {
@@ -11,6 +11,8 @@ const exposedApi = {
   getSubjects,
   getSubjectValue,
   getTopics,
+  getTopicConfig,
+  getTopicOffsets,
   getAwsSsmParams: getAwsSsmParams,
   getAwsSecrets: getAwsSecrets,
   prettify,

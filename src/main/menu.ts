30477968515d7a2f9030a7c0dddc4f04752f2a8d
Ewan Morrison <ewanmorrrison@gmail.com>
2022-07-14 18:33:19 +0100

    feat: add menu for copy+paste etc

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,23 +1,46 @@
 import { Menu, BrowserWindow } from "electron";
 
-export const configureMenu = (mainWindow: BrowserWindow) => {
-  mainWindow.setMenu(null);
+const menuItems: Electron.MenuItemConstructorOptions[] = [
+  { label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo" },
+  { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo" },
+  { type: "separator" },
+  { label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut" },
+  { label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy" },
+  { label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste" },
+  { label: "Select All", accelerator: "CmdOrCtrl+A", role: "selectAll" },
+];
 
+const getMenuItems = (
+  mainWindow: BrowserWindow,
+  x: number,
+  y: number
+): Electron.MenuItemConstructorOptions[] => {
   if (
     process.env.NODE_ENV === "development" ||
     process.env.DEBUG_PROD === "true"
   ) {
-    mainWindow.webContents.on("context-menu", (_, props) => {
-      const { x, y } = props;
-
-      Menu.buildFromTemplate([
-        {
-          label: "Inspect element",
-          click: () => {
-            mainWindow.webContents.inspectElement(x, y);
-          },
+    return [
+      ...menuItems,
+      { type: "separator" },
+      {
+        label: "Inspect element",
+        click: () => {
+          mainWindow.webContents.inspectElement(x, y);
         },
-      ]).popup({ window: mainWindow });
-    });
+      },
+    ];
   }
+  return menuItems;
+};
+
+export const configureMenu = (mainWindow: BrowserWindow) => {
+  mainWindow.setMenu(null);
+
+  mainWindow.webContents.on("context-menu", (_, props) => {
+    const { x, y } = props;
+
+    Menu.buildFromTemplate(getMenuItems(mainWindow, x, y)).popup({
+      window: mainWindow,
+    });
+  });
 };
